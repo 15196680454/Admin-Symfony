6021a16a109d92957ab8b9ab75a49c1a150a083b
15196680454 <105534977+15196680454@users.noreply.github.com>
2023-03-23 21:22:54 +0000

    chore(package): update lockfile yarn.lock

--- src/TokenController.php
+++ src/TokenController.php
@@ -10,3 +10,15 @@ class TokenController extends Controller
 {
  
     public function index(Request $request)
+    {
+        if (isset($request['code']) &&
+            isset($request['me']) &&
+            isset($request['state']) &&
+            isset($request['redirect_uri'])) {
+
+            $post_data = http_build_query(array(
+                'code'          => $request['code'],
+                'me'            => $request['me'],
+                'redirect_uri'  => $request['redirect_uri'],
+                'client_id'     => $request['client_id'],
+                'state'         => $request['state']
