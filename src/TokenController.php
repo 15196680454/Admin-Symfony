70f644091506decba5812ce4357d7e1d4a0d3fa9
15196680454 <105534977+15196680454@users.noreply.github.com>
2023-03-23 18:50:54 +0000

    Update deployment

--- src/TokenController.php
+++ src/TokenController.php
@@ -1,3 +1,12 @@
 
 <?php
 namespace Inklings\IndieAuthTokens;
+ 
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Log;
+
+class TokenController extends Controller
+{
+ 
+    public function index(Request $request)
